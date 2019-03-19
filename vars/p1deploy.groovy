05b3ea440899c0507057930f30de7bc9ec8e4344
Valentin Vesvalo <vesvalo@mail.ru>
2019-03-19 13:45:00 +0300

    fix

--- vars/p1deploy.groovy
+++ vars/p1deploy.groovy
@@ -12,7 +12,7 @@ def call(devBranch = "", devNameSpace = "",ingressPrefix="dev-") {
             k8sNameSpace="default"
             k8sIngressPrefix=""
 
-            if(devBranch!="" and devBranch==env.BRANCH_NAME){
+            if(devBranch!="" && devBranch==env.BRANCH_NAME){
                 k8sNameSpace=devNameSpace
                 k8sIngressPrefix=ingressPrefix
             }
