27625af28836f340d81a114fedd85b7c73865d00
Vladimir Stafievsky <justdewil@gmail.com>
2019-08-12 15:19:52 +0300

    Update p1secscan.groovy

--- vars/p1secscan.groovy
+++ vars/p1secscan.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Running aqua microscanner"
     BR_NAME=env.BRANCH_NAME
-    BR_NAME=BR_NAME.replaceAll("/","-")
+    BR_NAME=BR_NAME.replaceAll("/","-").toLowerCase()
     aquaMicroscanner imageName: "$CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID", notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
 }
