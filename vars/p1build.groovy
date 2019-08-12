a181cc52d71ad5cd46049b797222110d989caaa6
Vladimir Stafievsky <justdewil@gmail.com>
2019-08-12 14:58:28 +0300

    Update p1build.groovy

--- vars/p1build.groovy
+++ vars/p1build.groovy
@@ -1,6 +1,9 @@
 def call() {
     echo "Start building image"
 
+        BR_NAME=env.BRANCH_NAME
+        BR_NAME=BR_NAME.replaceAll("/","_")
+    
         script {
                     if(params.PROD_RELEASE){
                         sh ''' echo "production release"'''              
@@ -9,20 +12,19 @@ def call() {
                     }
                     checkout scm
                     sh '''
-                    BR_NAME=`echo $BRANCH_NAME | sed 's/\//_/g'`
-                    docker build -t $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID .
-                    (if [ -f Dockerfile.nginx ]; then docker build -t $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
+                    docker build -t $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID .
+                    (if [ -f Dockerfile.nginx ]; then docker build -t $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
                     '''
                 }
     echo "Pushing image"
         script {
+
                     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'p1docker',
                     usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD']]) {
                     sh '''
-                        BR_NAME=`echo $BRANCH_NAME | sed 's/\//_/g'`
                         docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-                        docker push $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID
-                        (if [ -f Dockerfile.nginx ]; then docker push $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID-static ; else echo "Project without static content"; fi);
+                        docker push $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID
+                        (if [ -f Dockerfile.nginx ]; then docker push $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID-static ; else echo "Project without static content"; fi);
                     '''
                     }
                 }
