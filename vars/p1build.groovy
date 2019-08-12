fc14f62743750684084966cfeca342dfef6fe963
Vladimir Stafievsky <justdewil@gmail.com>
2019-08-12 14:46:05 +0300

    Update p1build.groovy

--- vars/p1build.groovy
+++ vars/p1build.groovy
@@ -9,8 +9,9 @@ def call() {
                     }
                     checkout scm
                     sh '''
-                    docker build -t $CI_REGISTRY_IMAGE:$BRANCH_NAME-$BUILD_ID .
-                    (if [ -f Dockerfile.nginx ]; then docker build -t $CI_REGISTRY_IMAGE:$BRANCH_NAME-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
+                    BR_NAME=`echo $BRANCH_NAME | sed 's/\//_/g'`
+                    docker build -t $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID .
+                    (if [ -f Dockerfile.nginx ]; then docker build -t $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
                     '''
                 }
     echo "Pushing image"
@@ -18,9 +19,10 @@ def call() {
                     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'p1docker',
                     usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD']]) {
                     sh '''
+                        BR_NAME=`echo $BRANCH_NAME | sed 's/\//_/g'`
                         docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-                        docker push $CI_REGISTRY_IMAGE:$BRANCH_NAME-$BUILD_ID
-                        (if [ -f Dockerfile.nginx ]; then docker push $CI_REGISTRY_IMAGE:$BRANCH_NAME-$BUILD_ID-static ; else echo "Project without static content"; fi);
+                        docker push $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID
+                        (if [ -f Dockerfile.nginx ]; then docker push $CI_REGISTRY_IMAGE:$BR_NAME-$BUILD_ID-static ; else echo "Project without static content"; fi);
                     '''
                     }
                 }
