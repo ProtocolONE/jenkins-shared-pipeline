42b5e8a8dd1950e783a3f5e19e48760795a37ec5
Valentin Vesvalo <vesvalo@mail.ru>
2019-09-14 19:50:31 +0300

    chown files created by make in DIND

--- vars/p1build.groovy
+++ vars/p1build.groovy
@@ -13,12 +13,12 @@ def call() {
                     BR_NAME=env.BRANCH_NAME
                     BR_NAME=BR_NAME.replaceAll("/","-").toLowerCase()
                     sh """
-                    
+                    JENKINS_GID=$(id -g)
                     if [ -f Makefile ]
                     then
-                        DIND=1 TAG=${BR_NAME}-$BUILD_ID make vendor
-                        DIND=1 TAG=${BR_NAME}-$BUILD_ID make build
-                        DIND=1 TAG=${BR_NAME}-$BUILD_ID make docker-image
+                        DIND=1 TAG=${BR_NAME}-$BUILD_ID DIND_UID=${env.UID} DIND_GUID=$JENKINS_GID make vendor
+                        DIND=1 TAG=${BR_NAME}-$BUILD_ID DIND_UID=${env.UID} DIND_GUID=$JENKINS_GID make build
+                        DIND=1 TAG=${BR_NAME}-$BUILD_ID DIND_UID=${env.UID} DIND_GUID=$JENKINS_GID make docker-image
                     else
                         docker build -t $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID .
                         (if [ -f Dockerfile.nginx ]; then docker build -t $CI_REGISTRY_IMAGE:${BR_NAME}-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
