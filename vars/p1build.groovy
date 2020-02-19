d367e4245fb6c166c43d68c10ec9a9a17be4f8cc
Vladimir Stafievsky <justdewil@gmail.com>
2020-02-19 11:45:00 +0300

    Branch tag to docker images added

--- vars/p1build.groovy
+++ vars/p1build.groovy
@@ -29,7 +29,7 @@ def call() {
                             GOPATH=/go DIND=1 TAG=${BR_NAME}-$BUILD_ID DIND_UID=$JENKINS_UID DIND_GUID=$JENKINS_GID make docker-image-jenkins
                         else
                             echo "REGISTRY_IMAGE: ${registryImage}"
-                            docker build -t ${registryImage}:${BR_NAME}-$BUILD_ID .
+                            docker build -t ${registryImage}:${BR_NAME}-$BUILD_ID -t ${registryImage}:${BR_NAME} .
                             (if [ -f Dockerfile.nginx ]; then docker build -t ${registryImage}:${BR_NAME}-$BUILD_ID-static -f Dockerfile.nginx . ; else echo "Project without static content"; fi);
                         fi
                     
@@ -50,6 +50,7 @@ def call() {
                             echo "CI_REGISTRY_IMAGE: ${registryImage}"
                             docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
                             docker push ${registryImage}:${BR_NAME}-$BUILD_ID
+                            docker push ${registryImage}:${BR_NAME}
                             (if [ -f Dockerfile.nginx ]; then docker push ${registryImage}:${BR_NAME}-$BUILD_ID-static ; else echo "Project without static content"; fi);
                         """
                     }
