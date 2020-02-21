08dcbf09598cb412156b9047c2d9c26a8a9a38e4
Vladimir Stafievsky <justdewil@gmail.com>
2020-02-21 17:29:01 +0300

    Hotfix

--- vars/p1custombranch.groovy
+++ vars/p1custombranch.groovy
@@ -4,8 +4,8 @@ def call() {
 
         def getBranches = ("git ls-remote -t -h ${repositoryUrl}").execute()
 
-        branchList = getBranches.text.readLines().collect {
-                it.split()[1].replaceAll('refs/heads/', ").replaceAll('refs/tags/', ").replaceAll("\\^\\{\\}", ")
+        def branchList = getBranches.text.readLines().collect {
+                it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\\^\\{\\}", '')
         }
 
         echo "List of project branches: ${branchList}"
