e89602fa388ddd19824828bbd90198798fedcaf8
Vladimir Stafievsky <justdewil@gmail.com>
2020-03-04 17:01:39 +0300

    tests

--- vars/p1custombranch.groovy
+++ vars/p1custombranch.groovy
@@ -1,6 +1,22 @@
-@NonCPS
 def call() {
     script {
+        try {
+            timeout(time:60, unit:'SECONDS') {
+                BRANCH_TO_BUILD_REQUESTED = input(
+                    message: 'Input branch to build', 
+                    parameters: [
+                        [$class: 'TextParameterDefinition', 
+                            //defaultValue: BRANCH_TO_BUILD_DEFAULT, 
+                            defaultValue: 'develop', 
+                            description: 'Branch name', name: 'Enter branch name (or leave default) and press [Proceed]:']
+                    ])
+                    echo ("User has entered the branch name: " + BRANCH_TO_BUILD_REQUESTED)
+            }
+        } catch(err) {
+            echo err.getMessage()
+            echo "Input aborted"
+        }
+        /*
         def repositoryUrl = scm.userRemoteConfigs[0].url
 
         def getBranches = ("git ls-remote -t -h ${repositoryUrl}").execute()
@@ -20,7 +36,7 @@ def call() {
                 ]
             ]
         
-        echo "You selected branch with name: ${selectBranch}"
+        echo "You selected branch with name: ${selectBranch}" */
 
         //def selectedBranch = sh (returnStdout: true, script: "echo ${selectBranch}")
         //echo "You selected branch with name: ${selectedBranch}"
