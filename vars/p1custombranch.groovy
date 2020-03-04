e481a2eb16d4aec3d2d0a3ade8a1485a7dbb7cb5
Vladimir Stafievsky <justdewil@gmail.com>
2020-03-04 14:41:07 +0300

    tests

--- vars/p1custombranch.groovy
+++ vars/p1custombranch.groovy
@@ -10,34 +10,18 @@ def call() {
         }
         echo "BranchList: ${BranchList}"
         
-        try {
-            timeout(time:60, unit:'SECONDS') {
-/*                def selectBranch = input message: 'Please select branch', ok: 'Next',
-                    parameters: [
-                        [
-                        $class: 'ChoiceParameterDefinition',
-                        name: 'BRANCH', 
-                        choices: BranchList,
-                        description: 'Project branches'
-                        ]
-                    ]
-            }*/
-                BRANCH_TO_BUILD_REQUESTED = input(
-                    message: 'Input branch to build', 
-                    parameters: [
-                        [$class: 'TextParameterDefinition', 
-                            //defaultValue: BRANCH_TO_BUILD_DEFAULT, 
-                            defaultValue: 'develop', 
-                            description: 'Branch name', name: 'Enter branch name (or leave default) and press [Proceed]:']
-                    ])
-                    echo ("User has entered the branch name: " + BRANCH_TO_BUILD_REQUESTED)
-            }
-        } catch(err) {
-            echo err.getMessage()
-            echo "Input aborted"
-        }
-        //def selectedBranch = sh (returnStdout: true, script: "echo ${selectBranch} | cut -d ' ' -f 1")
-        //echo "You selected branch with name: ${selectBranch}"
+        def selectBranch = input message: 'Please select branch', ok: 'Next',
+            parameters: [
+                [
+                $class: 'ChoiceParameterDefinition',
+                name: 'BRANCH', 
+                choices: BranchList,
+                description: 'Project branches'
+                ]
+            ]
+
+        def selectedBranch = sh (returnStdout: true, script: "echo ${selectBranch} | cut -d ' ' -f 1")
+        echo "You selected branch with name: ${selectBranch}"
         //env.BRANCH_NAME=selectedBranch
     }
 }
