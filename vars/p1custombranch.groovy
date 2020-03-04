b69db033a50025f8607cccba1613ce7b1598bc49
Vladimir Stafievsky <justdewil@gmail.com>
2020-03-04 16:54:20 +0300

    tests

--- vars/p1custombranch.groovy
+++ vars/p1custombranch.groovy
@@ -1,34 +1,4 @@
 @NonCPS
-def selectBranch() {
-    try {
-        timeout(time:60, unit:'SECONDS') {
-/*                def selectBranch = input message: 'Please select branch', ok: 'Next',
-                parameters: [
-                    [
-                    $class: 'ChoiceParameterDefinition',
-                    name: 'BRANCH', 
-                    choices: BranchList,
-                    description: 'Project branches'
-                    ]
-                ]
-        }*/
-
-        BRANCH_TO_BUILD_REQUESTED = input(
-                message: 'Input branch to build', 
-                parameters: [
-                    [$class: 'TextParameterDefinition', 
-                        //defaultValue: BRANCH_TO_BUILD_DEFAULT, 
-                        defaultValue: 'develop', 
-                        description: 'Branch name', name: 'Enter branch name (or leave default) and press [Proceed]:']
-                ])
-                echo ("User has entered the branch name: " + BRANCH_TO_BUILD_REQUESTED)
-        }
-    } catch(err) {
-        echo err.getMessage()
-        echo "Input aborted"
-    }
-}
-
 def call() {
     script {
         def repositoryUrl = scm.userRemoteConfigs[0].url
@@ -39,8 +9,8 @@ def call() {
                 it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\\^\\{\\}", '')
         }
         echo "BranchList: ${BranchList}"
-        selectBranch();
-/*        def selectBranch = input message: 'Please select branch', ok: 'Next',
+        
+        selectBranch = input message: 'Please select branch', ok: 'Next',
             parameters: [
                 [
                 $class: 'ChoiceParameterDefinition',
@@ -48,9 +18,9 @@ def call() {
                 choices: BranchList,
                 description: 'Project branches'
                 ]
-            ]*/
+            ]
         
-        //echo "You selected branch with name: ${selectBranch}"
+        echo "You selected branch with name: ${selectBranch}"
 
         //def selectedBranch = sh (returnStdout: true, script: "echo ${selectBranch}")
         //echo "You selected branch with name: ${selectedBranch}"
